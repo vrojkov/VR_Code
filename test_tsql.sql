28ee82c3edee70dfae90069739a0edc6d539753f
Rojcov Vladimir <n1203739a@am.munichre.com>
2018-03-07 17:52:14 -0500

    Color theme

---
+++ test_tsql.sql
@@ -0,0 +1,7 @@
+DECLARE @sn NVARCHAR(128);
+EXEC master.dbo.xp_regread
+    'HKEY_LOCAL_MACHINE',
+    'SYSTEM\CurrentControlSet\services\SQLSERVERAGENT',
+    'ObjectName', 
+    @sn OUTPUT;
+SELECT @sn;
