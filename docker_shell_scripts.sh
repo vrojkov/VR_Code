6ebe16a9a4c51371a2ae64e1395eebcf6d572e53
vrojkov <37094242+vrojkov@users.noreply.github.com>
2018-03-13 16:32:47 -0400

    Add files via upload

---
+++ docker_shell_scripts.sh
@@ -0,0 +1,57 @@
+
+#Download SQL docker:
+sudo docker pull microsoft/mssql-server-linux:2017-latest
+
+#Check existing dockers:
+docker ps -a
+
+#Sql Docker start when image doesn't exists - i.e. create new container with name sql_VR_1:
+sudo docker run -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=HarfordSteam1!' \
+   -p 4433:1433 --name sql_VR_1 \
+   -d microsoft/mssql-server-linux:2017-latest
+
+ #If image exists but container is not running -> Start it:
+ #Stop container
+sudo docker start sql_VR_1   
+
+#Sleep 30 sec - it takes time for sql to start
+Sleep 30
+
+#Stop container
+sudo docker stop sql_VR_1 
+
+#Remove container. THis actually doesn't remove image file
+sudo docker rm sql_VR_1
+
+#THis removes docker image file from local repo
+docker images #-- Get IMAGE_ID
+sudo docker rmi 91ac25f0495b
+
+#------- Save cintainer into image with your changes
+
+#list images. Check Container ID which you are working on
+docker images
+docker ps -a
+
+#-- Create new image localy (or you can push it to GitHub at creation)
+docker commit 31b78336ee2c vrojkov/mssql-server-linux_testdb_01:2017-Test_DB_01
+
+
+#-- check result
+docker images
+docker ps -a
+
+#-- Start SQL using new image. Some params not required, like sa pwd and ACCEPT_EULA, you can even skip port# and name
+sudo docker run -p 4433:1433 --name sql_VR_Test_DB_01 \
+   -d vrojkov/mssql-server-linux_testdb_01:2017-Test_DB_01
+
+
+#-- Push new image to DockerHub repo. Using username and pwd. Altrantivelly can use your e-mail in the command string instead of pwd 
+docker login --username=vrojkov
+
+docker push  vrojkov/mssql-server-linux_testdb_01:2017-Test_DB_01
+
+#-- Pull image from GitHub
+docker pull vrojkov/mssql-server-linux_testdb_01:2017-Test_DB_01
+
+#-- Change
