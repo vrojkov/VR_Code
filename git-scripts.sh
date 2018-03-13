06efbe6e37d020f34bdedce40c4788c1c6559167
Rojcov Vladimir <n1203739a@am.munichre.com>
2018-03-13 12:48:33 -0400

    git-scripts.sh added

---
+++ git-scripts.sh
@@ -0,0 +1,4 @@
+git remote add origin https://github.com/vrojkov/VR_Code
+git remote show origin # if everything is ok, you will see your remote
+
+git push -u origin vr-developer-machine  #master # assuming your are on the master branch.
